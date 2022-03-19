466e66c35cbfcda82872cb52ce35b1999e1e9e1c
Bhavana N <96416690+BhavanaN30@users.noreply.github.com>
2022-03-19 08:26:22 +0000

    p5original.c

--- p5original.c
+++ p5original.c
@@ -1,17 +1,52 @@
 #include<stdio.h>
+
+int input_array_size()
+{
+  int a;
+  printf("Enter the array size :\n");
+  scanf("%d", &a);
+  return a;
+}
+
+void init_array(int n, int a[n])
+{
+  int i;
+  for(i=0; i<n; i++)
+    {
+      a[i]=i+1;
+    }
+}
+
+void erotosthenes_seive(int n, int a[n])
+{
+  int i,j;
+  for(i=1; i<n; i++)
+    {
+      if(a[i]==0)
+        continue;
+      for(j=a[i]+1; j<=n; j++)
+        {
+          if(j%a[i]==0)
+            a[j-1]=0;
+        }
+    }
+}
+
+void output(int n, int a[n])
+{
+  int i;
+  printf("Prime numbers are : \n");
+   for(i=0; i<n; i++)
+    printf("%d ",a[i]);
+}
+
 int main()
 {
   int n;
-  printf("enter n value\n");
-  scanf("%d",&n);
-  int prime[n+1];
-  
-  
-
-
-
-
-
-
-
-
+  n=input_array_size();
+  int a[n];
+  init_array(n,a);
+  erotosthenes_seive(n,a);
+  output(n,a);
+  return 0;
+}
